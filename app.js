fe568a70d3722bb69dd0f849f2517af845bb4251
William Vuong <wnvuong@gmail.com>
2017-12-09 21:50:00 -0500

    implemented s3 policy generation

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const { s3Credentials } = require('./s3credentials');
+const crypto = require('crypto');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const port = 4000;
+const config = JSON.parse(fs.readFileSync('./s3config.json', 'utf8'));
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/api/s3credentials', (req, res) => {
+  if (req.query.filename) {
+    var filename =
+      crypto.randomBytes(16).toString('hex') + path.extname(req.query.filename);
+    res.json(
+      s3Credentials(config, {
+        filename: filename,
+        contentType: req.query.content_type
+      })
+    );
+  } else {
+    res.status(400).send('filename is required');
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => console.log(`Listening on port ${port}!`));
